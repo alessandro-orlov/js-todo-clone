4fee636ffc0dc17944c2b536dbe7aaa330d8a82d
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-06-15 20:48:17 +0200

    aggiunto keypress function all'input

--- js/myscript.js
+++ js/myscript.js
@@ -18,6 +18,23 @@ $(document).ready(
       taskValue = $('input.task').val('');
     });
 
+    // Premendo il tasto invio inserisco il valore dell'input alla lista
+    $('.task').keypress(function(event) {
+      if(event.which === 13) {
+      // Prendo il vaolre dell'input
+      var taskValue = $('input.task').val();
+      // Clone tamplate dal DOM html
+      var cloneLi = $('.tamplate > li').clone();
+      // Aggiungo al Tamplate il valore del input
+      cloneLi.prepend(taskValue);
+
+      // Inserisco l'elemento nella lista
+      $('.lista-task').append(cloneLi);
+
+      // Resetto il valore iniziale (vuoto) dell'input
+      taskValue = $('input.task').val('');
+      }
+    });
 
     // Cancello l'elemento dalla lista
     $(document).on('click', '.delete-task-element', function() {
