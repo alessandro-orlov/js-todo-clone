67ba23321ea651d81015ae62b49d385cd00a039d
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-06-15 20:54:10 +0200

    Creato la funzione per semplificare il codice JS

--- js/myscript.js
+++ js/myscript.js
@@ -1,38 +1,23 @@
 $(document).ready(
   function() {
 
-
     // Al click prendo il valore del input .task e lo aggiungo alla lista
     $('.add-task-btn-js').click(function() {
       // Prendo il vaolre dell'input
       var taskValue = $('input.task').val();
-      // Clone tamplate dal DOM html
-      var cloneLi = $('.tamplate > li').clone();
-      // Aggiungo al Tamplate il valore del input
-      cloneLi.prepend(taskValue);
 
-      // Inserisco l'elemento nella lista
-      $('.lista-task').append(cloneLi);
-
-      // Resetto il valore iniziale (vuoto) dell'input
-      taskValue = $('input.task').val('');
+      // Aggiungo il tamplate con la funzione
+      addElement(taskValue);
     });
 
     // Premendo il tasto invio inserisco il valore dell'input alla lista
     $('.task').keypress(function(event) {
       if(event.which === 13) {
-      // Prendo il vaolre dell'input
-      var taskValue = $('input.task').val();
-      // Clone tamplate dal DOM html
-      var cloneLi = $('.tamplate > li').clone();
-      // Aggiungo al Tamplate il valore del input
-      cloneLi.prepend(taskValue);
+        // Prendo il vaolre dell'input
+        var taskValue = $('input.task').val();
 
-      // Inserisco l'elemento nella lista
-      $('.lista-task').append(cloneLi);
-
-      // Resetto il valore iniziale (vuoto) dell'input
-      taskValue = $('input.task').val('');
+        // Aggiungo il tamplate con la funzione
+        addElement(taskValue);
       }
     });
 
@@ -42,3 +27,21 @@ $(document).ready(
     });
 
   }); // End document .ready
+
+  // =========================================================
+  // ================== FUNCTIONS ============================
+
+  // Function aggiungi l'elemento con il tamplate
+  function addElement(value) {
+    // Clone tamplate dal DOM html
+    var cloneLi = $('.tamplate > li').clone();
+
+    // Aggiungo al Tamplate il valore del input
+    cloneLi.prepend(value);
+
+    // Inserisco l'elemento nella lista
+    $('.lista-task').append(cloneLi);
+
+    // Resetto il valore iniziale (vuoto) dell'input
+    value = $('input.task').val('');
+  }
